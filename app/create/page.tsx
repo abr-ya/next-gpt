a9500cd917e9c129a490beafd2646cce1a97d535
abr-ya <gamer05@list.ru>
2023-06-16 08:19:23 +0300

    fix: Add UserId to Create request

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,32 +1,43 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Form } from "../components";
-import { IFormData } from "../interfaces";
+import { IFormData, ISessionUser } from "../interfaces";
 
 const Page = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   const [loading, setLoading] = useState(false);
 
   const createPrompt = (data: IFormData): void => {
     console.log(data);
-    setLoading(true);
+    const user = session?.user as ISessionUser;
 
-    // todo: CreateRequest
-    axios
-      .post("/api/prompt", data)
-      .then(() => {
-        toast.success("Prompt and tag are saved!)");
-        // router.push("/trips");
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error("Something went wrong!(");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    if (user?.id) {
+      setLoading(true);
+
+      // todo: CreateRequest
+      axios
+        .post("/api/prompt", { ...data, userId: user.id })
+        .then(() => {
+          toast.success("Prompt and tag are saved!)");
+          router.push("/");
+        })
+        .catch((err) => {
+          console.log(err);
+          toast.error("Something went wrong!(");
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      toast.error(`Can't create post - ${user?.id} userId!(`);
+    }
   };
 
   return <Form type="Create" loading={loading} handleSubmit={createPrompt} />;
--- app/interfaces.ts
+++ app/interfaces.ts
@@ -1,3 +1,10 @@
+export interface ISessionUser {
+  email: string;
+  id: string;
+  image: string;
+  name: string;
+}
+
 export interface IFormData {
   prompt: string;
   tag?: string;
